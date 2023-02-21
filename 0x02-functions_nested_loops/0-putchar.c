0be87e839e42b53317d97ac486e1748f5a3aab45
MikkyJay7 <michaelsjoseph21@gmail.com>
2023-02-21 12:10:32 -0800

    3-islower.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,16 +1,23 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
- * main - entry code
+ * main - Prints "Holberton" followed by a new line.
  *
- * program that prints _putchar
- * followed by new line
- *
- * Return:  returns 0
- **/
-
+ * Return: Always 0.
+ */
 int main(void)
 {
-	puts("_putchar");
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchar('\n');
+
 	return (0);
 }
+
