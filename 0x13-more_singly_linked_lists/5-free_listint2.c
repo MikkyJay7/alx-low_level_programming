b7f8107637daf01c5d4e4703da94cd641029b8ee
MikkyJay7 <michaelsjoseph21@gmail.com>
2023-04-05 06:33:07 -0700

    Mandatory tasks done

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,27 @@
+/* 
+ * File: 5-free_listint2.c
+ * Auth: Michael Joseph
+ */
+
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a linked list
+ * @head: pointer to the listint_t list to be freed
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *tmp;
+
+	if (head == NULL)
+		return;
+
+	while (*head)
+	{
+		tmp = (*head)->next;
+		free(*head);
+		*head = tmp;
+	}
+
+	*head = NULL;
+}
